0aed7bb26dc24474b097b13bf154ed8e6475a0ab
Santiago Millé <103786368+Pomelo4455@users.noreply.github.com>
2025-01-23 12:07:05 -0300

    Stringified the logged JSON

--- api/webhook.js
+++ api/webhook.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
   if (req.method === 'POST') {
-    console.log('Received payload:', req.body);
+    console.log('Received payload:', JSON.stringify(req.body));
     res.status(200).send('Webhook received');
   } else {
     res.status(404).send('Not Found');
