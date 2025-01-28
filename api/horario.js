db081eb5eab9ed0a7b4707aa60aa98a41e21c66b
Santiago Millé <103786368+Pomelo4455@users.noreply.github.com>
2025-01-28 11:36:59 -0300

    Added schedule time for testing

---
+++ api/horario.js
@@ -0,0 +1,14 @@
+export default function handler(req, res) {
+    const guatemalaTime = new Date().toLocaleString("en-US", {
+      timeZone: "America/Guatemala",
+    });
+  
+    const currentHour = new Date(guatemalaTime).getHours();
+  
+    if (currentHour >= 6 && currentHour < 11) {
+      return res.status(200).json({ result: "DESAYUNO" });
+    } else {
+      return res.status(200).json({ result: "ALMUERZO" });
+    }
+  }
+  
