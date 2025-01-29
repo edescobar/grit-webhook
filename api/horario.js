de3b8ff43249833f4e32de303ef6f67baf9680a2
Santiago Millé <103786368+Pomelo4455@users.noreply.github.com>
2025-01-29 16:46:34 -0300

    Added log

--- api/horario.js
+++ api/horario.js
@@ -1,14 +1,15 @@
 export default function handler(req, res) {
-    const guatemalaTime = new Date().toLocaleString("en-US", {
-      timeZone: "America/Guatemala",
-    });
-  
-    const currentHour = new Date(guatemalaTime).getHours();
-  
-    if (currentHour >= 6 && currentHour < 11) {
-      return res.status(200).json({ result: "DESAYUNO" });
-    } else {
-      return res.status(200).json({ result: "ALMUERZO" });
-    }
+  const guatemalaTime = new Date().toLocaleString("en-US", {
+    timeZone: "America/Guatemala",
+  });
+
+  console.log("Time: ", guatemalaTime);
+
+  const currentHour = new Date(guatemalaTime).getHours();
+
+  if (currentHour >= 6 && currentHour < 11) {
+    return res.status(200).json({ result: "DESAYUNO" });
+  } else {
+    return res.status(200).json({ result: "ALMUERZO" });
   }
-  
+}
